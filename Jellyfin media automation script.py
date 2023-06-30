57819c045b25484e2b71f4ddf4f6a00881ee1fca
ElGuapo <138128423+Elguapo2005@users.noreply.github.com>
2023-06-30 19:42:05 +0530

    Update Jellyfin media automation script.py

--- Jellyfin media automation script.py
+++ Jellyfin media automation script.py
@@ -1,4 +1,4 @@
-import os
+ import os
 import requests
 import schedule
 import shutil
@@ -41,10 +41,18 @@ def fetch_data(url: str, params: Dict = None) -> List[Dict]:
         return []
     return response.json()
 
-# ... (rest of the code)
+def fetch_libraries() -> List[Dict]:
+    """ Fetches the list of libraries from Jellyfin. """
+    url = f"{config['jellyfin_url']}/Users/{config['user_id']}/Items"
+    params = {
+        "Recursive": True,
+        "IncludeItemTypes": "Library",
+        "Fields": "BasicSyncInfo"
+    }
+    return fetch_data(url, params)
 
-def move_files(library_id):
-    """ Move all the watched items for a given library to the backup folder. """
+def move_tv_show_files(library_id):
+    """ Move all the watched TV show episodes for a given library to the backup folder. """
     validate_config()
     watched_items = fetch_watched_items(library_id)
     seasons = fetch_seasons(library_id)
@@ -99,6 +107,50 @@ def move_files(library_id):
         # Remove the file from the previous library
         remove_from_library(item)
 
+def move_movie_files(library_id):
+    """ Move the watched movie file along with its containing folder for a given library to the backup folder. """
+    validate_config()
+    watched_items = fetch_watched_items(library_id)
+
+    for item in watched_items:
+        if is_paused:
+            logging.info("Backup process is paused. Resuming in 5 minutes...")
+            time.sleep(300)  # Pause for 5 minutes before resuming
+            continue
+        
+        file_path = item["Path"]
+        full_path = os.path.join("/home/user/videos", file_path)
+
+        # Check if the file format is supported
+        if not is_supported_format(file_path):
+            logging.info(f"Skipping file '{file_path}' as it is not in a supported format.")
+            continue
+
+        # Check if the destination folder exists
+        dest_folder_path = os.path.join(config["backup_folder_path"], library_id)
+        if not os.path.exists(dest_folder_path):
+            try:
+                os.makedirs(dest_folder_path)
+            except OSError as e:
+                logging.error(f"Error creating destination folder: {e}")
+                return
+
+        # Check if the file is already in the destination folder
+        dest_file_path = os.path.join(dest_folder_path, os.path.basename(file_path))
+        if os.path.exists(dest_file_path):
+            logging.info(f"File already exists in the destination folder: {dest_file_path}")
+            return
+
+        # Move the file along with its containing folder
+        try:
+            shutil.move(os.path.dirname(full_path), dest_folder_path)
+        except shutil.Error as e:
+            logging.error(f"Error moving file and its folder: {e}")
+            raise
+
+        # Remove the file from the previous library
+        remove_from_library(item)
+
 def remove_from_library(item):
     """ Removes the given item from the library using the Jellyfin API. """
     item_id = item["Id"]
@@ -115,12 +167,19 @@ def is_supported_format(file_path):
     _, ext = os.path.splitext(file_path)
     return ext.lower() in config["supported_formats"]
 
-# ... (rest of the code)
-
 if __name__ == "__main__":
     # Schedule the backup process
-    schedule.every().day.at("02:00").do(move_files, library_id="12345")  # Schedule at 2 AM
-    schedule.every().day.at("07:00").do(move_files, library_id="12345")  # Schedule at 7 AM
+    libraries = fetch_libraries()
+    for library in libraries:
+        library_type = library["Type"]
+        library_id = library["Id"]
+        
+        if library_type == "Movie":
+            schedule.every().day.at("02:00").do(move_movie_files, library_id=library_id)  # Schedule movie backup at 2 AM
+        elif library_type == "Series":
+            schedule.every().day.at("07:00").do(move_tv_show_files, library_id=library_id)  # Schedule TV show backup at 7 AM
+        else:
+            logging.warning(f"Skipping library '{library_id}'. Unsupported library type: {library_type}")
 
     logging.info("Backup script is running. Press Ctrl+C to exit.")
 
